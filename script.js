7086b35489197fdfc0d8604d66925f22a9b90008
Ayva Valenti <ayvavalenti@Ayvas-MacBook-Air.local>
2022-03-13 16:29:31 -0400

    trying all the things

--- script.js
+++ script.js
@@ -4,24 +4,21 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var password=document.getElementById("password");
 
 // Write password to the #password input
 function writePassword() {
     var characters = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()-_=+/?";
-    var passLength = 18;
+    var passLength = Math.floor(Math.random()* 121) + 8;
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+for (var i = 0; i <= passLength; i++) {
+    
+    }
 }
 
-for (var i = 0; i <= passLength; i++) {
-    var newPassword = Math.floor(Math.random () * characters.length);
-    password += characters.substring(newPassword, newPassword + 1);
-}
-var password=document.getElementById("password").value = password;
 // Add event listener to generate button
 
 generateBtn.addEventListener("click", writePassword);
