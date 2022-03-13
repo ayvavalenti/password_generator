af166e19478b10181a58ce15f27fa93a6d450929
Ayva Valenti <ayvavalenti@Ayvas-MacBook-Air.local>
2022-03-13 15:48:27 -0400

    button won't work

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ var password=document.getElementById("password");
 
 // Write password to the #password input
 function writePassword() {
-    var characters = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()";
-    var passwordSize = 10;
+    var characters = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()-_=+/?";
+    var passLength = 18;
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
 
@@ -17,8 +17,11 @@ function writePassword() {
 
 }
 
-for (var i = 0; i <= passwordSize; i++) {
-
+for (var i = 0; i <= passLength; i++) {
+    var newPassword = Math.floor(Math.random () * characters.length);
+    password += characters.substring(newPassword, newPassword + 1);
 }
+var password=document.getElementById("password").value = password;
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
